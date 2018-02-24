c4f39347368bcc59ba98d5c5f5c18d190592472a
Giorgi <giorgi.kharshila@gmail.com>
2018-02-24 15:04:28 -0500

    Course Class final

--- minConflict/course.py
+++ minConflict/course.py
@@ -5,16 +5,19 @@ import math
 from datetime import time
 
 class Course():
-	def __init__(self, title, duration, frequency, proffessor, level, importanceIndex = 0, maxStudents, numEnrolled = 0):
+	def __init__(self, ID, courseNumber, title, duration, frequency, proffessor, level, importanceIndex = 0, numEnrolled):
+		
+		self.id = ID
+		self.courseNumber = courseNumber
 		self.title = title
 		self.duration = duration
 		self.frequency = frequency
 		self.proffessor = proffessor
 		self.level = level #3 = 300 level course
 		self.importanceIndex = importanceIndex #geometric mean of weights given by students
-		self.maxStudents = maxStudents
 		self.numEnrolled = numEnrolled #actual number of students enrolled
 		self.notAvailableAt = []
+		self.finalSchedule = []
 
 	#Formulae for importanceIndex: sum / sqrt(numEnrolled)
 	def incrementImportanceIndex(self, weight):
