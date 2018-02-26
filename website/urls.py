88d793065a16ee0a725969c8e2518125ed2df3fa
Giorgi <giorgi.kharshila@gmail.com>
2018-02-25 22:43:14 -0500

    django admin url modified

--- website/urls.py
+++ website/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from website import views
 
 urlpatterns = [
-	url(r'^$', views.admin, name='index'),
+	url(r'^$', views.index, name = "index"),
+	url(r'^dashboard/$', views.dashboard, name='dashboard'),
 	url(r'^login/$', views.login, name='login')
 ]
--- website/views.py
+++ website/views.py
@@ -3,12 +3,14 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+def index(request):
+	return render(request, 'index.html')
 
 def login(request):
 	return render(request, 'login.html')
 
 # @login_required(login_url='/login/')
-def admin(request):
+def dashboard(request):
 	# if request.user.is_authenticated:
 	if True:
 		return render(request, 'index.html')
