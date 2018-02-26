eef2f82be5576a0c0612aa0d5ac1d6dbacf96378
Giorgi <giorgi.kharshila@gmail.com>
2018-02-25 23:23:29 -0500

    django customized login form

--- website/urls.py
+++ website/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
-from website import views
+from website.views import *
 
 urlpatterns = [
-	url(r'^$', views.index, name = "index"),
-	url(r'^dashboard/$', views.dashboard, name='dashboard'),
-	url(r'^login/$', views.login, name='login')
+	url(r'^$', index_view, name = "index"),
+	url(r'^dashboard/$', dashboard_view, name='dashboard'),
+	url(r'^login/$', login_view, name='login'),
+	url(r'^logout/$', logout_view, name='logout'),
+	url(r'^register/$', register_view, name='register')
 ]
--- website/views.py
+++ website/views.py
@@ -1,18 +1,27 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, get_user_model, login, logout
+
+from website.forms import UserLoginForm
 
 # Create your views here.
 
-def index(request):
+def index_view(request):
 	return render(request, 'index.html')
 
-def login(request):
+def login_view(request):
+	form = UserLoginForm(request.POST or None)
+
+	if form.is_valid():
+		email 		= form.cleaned_data.get("email")
+		password 	= form.cleaned_data.get("password")
+
+	return render(request, 'login.html', {"form": form})
+
+def register_view(request):
+	return render(request, 'register.html')
+
+def logout_view(request):
 	return render(request, 'login.html')
 
-# @login_required(login_url='/login/')
-def dashboard(request):
-	# if request.user.is_authenticated:
-	if True:
-		return render(request, 'index.html')
-	else:
-		return redirect('login')
+def dashboard_view(request):
+	return render(request, 'index.html')
