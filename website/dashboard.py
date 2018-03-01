45330d7b97d3417033f5f0524073fa5dcd1e3f26
Giorgi <giorgi.kharshila@gmail.com>
2018-03-01 14:06:56 -0500

    preferred courses modified

--- website/dashboard.py
+++ website/dashboard.py
@@ -56,8 +56,33 @@ class Dashboard(object):
 
 	def getPreferredCourses(self):
 		preferred_courses = self.getStudent()['preferred_courses'] or "n/a"
-		json_courses = json.loads(preferred_courses)['preferred_courses'][0]
-		return json_courses
+		if preferred_courses == "n/a":
+			return []
+
+		course_dict = {}
+		course_list = preferred_courses.split(",")[:-1]
+		colors = ['red', 'pink', 'purple', 'blue', 'indigo', 'lime', 'orange', 'brown', 'grey', 'grey darken-4']
+
+		for course in course_list:
+			course_dict[course] = self.getCourse(course)
+		
+		for i in range(len(course_list)):
+			c = course_list[i]
+			course_dict[c][c]["priority"] = i+1
+			course_dict[c][c]["percent"] = (10-i)*10
+			course_dict[c][c]["color"] = colors[i]
+
+		return course_dict.items()
+
+	def getCourse(self, course):
+		course_dict = {}
+
+		cursor 	= connection.cursor()
+		cursor.execute("SELECT * FROM courses WHERE crn=%s", [course])
+		desc = cursor.description
+		course_dict[course] = [dict(zip([col[0] for col in desc], row)) for row in cursor.fetchall()][0]
+		return course_dict
+
 
 	def getFinalCourses(self):
 		preferred_courses = self.getStudent()['final_courses'] or "n/a"
