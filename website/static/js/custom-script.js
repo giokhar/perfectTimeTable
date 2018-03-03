f8f50addccff2a334a73d27cf4b8c80278ad3ab1
Giorgi <giorgi.kharshila@gmail.com>
2018-03-03 03:50:01 -0500

    add courses with jQuery

--- website/static/js/custom-script.js
+++ website/static/js/custom-script.js
@@ -9,3 +9,13 @@ NOTE:
 ------
 PLACE HERE YOUR OWN JS CODES AND IF NEEDED.
 WE WILL RELEASE FUTURE UPDATES SO IN ORDER TO NOT OVERWRITE YOUR CUSTOM SCRIPT IT'S BETTER LIKE THIS. */
+
+$(function() {
+	counter = 1;
+	$(".add").click(function () {
+	    if (counter < 10){ // Check not to add duplicate items
+	  		$("#courses").append($(this).val()+"<br>");
+	  	}
+	  	counter++;
+	});
+});
